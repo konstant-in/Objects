1e6b63fd15423e63a605a33a4c5fd3ee07ec3743
Konstantin Kulikin <konstantin2000@gmail.com>
2020-01-07 12:58:04 +0200

    1

--- s4.py
+++ s4.py
@@ -12,7 +12,7 @@ class Class2(Class1): # Класс Class2 наследует класс Class1
 c = Class2() # Создаем экземпляр класса Class2
 c.func1() # Выведет: Метод func1() базового класса Class1
 c.func2() # Выведет: Метод func2() базового класса Class1
-c.func3() # Выведет: Метод func3() "своего" класса Class2
+c.func3() # Выведет: Метод func3() производного класса Class2
 
 print(c.x) # Обращение к атрибуту базоаого класса Class1
 print(c.__dict__)
